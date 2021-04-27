185418bdb65313cd3c91a75b3be83585e759df22
jenleebeaver <jen.lee.beaver@gmail.com>
2021-04-26 18:23:09 -0700

    commit tree b6241c8b67a96bb47edeaa0289a00733cfc08856

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import RestaurantInput from './components/RestaurantInput';
 import Restaurants from './components/Restaurants';
+import { addRestaurant } from './actions/restaurants';
 
 export class App extends Component {
   render() {
@@ -13,4 +15,4 @@ export class App extends Component {
   }
 };
 
-export default App;
+export default connect(state => ({ items: state.items }), { addRestaurant })(App);
